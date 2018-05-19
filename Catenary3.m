fdce38d8532bbaf17b15b6301caf8748188b48ec
Dragos Oprea <opread@gmail.com>
2018-05-19 14:38:19 +0300

    Catenary beehive shape

--- Catenary3.m
+++ Catenary3.m
@@ -1,3 +1,9 @@
+# Dadant
+# top frame: 470 / 435 X 27
+# frame height: 37 to 27 X 300 X 6
+# |----|
+# |____|
+ 
 % Prevent Octave from thinking that this
 % is a function file:
 1;
@@ -11,32 +17,33 @@ function [x, y] = catenary(s,w,a)
 end
 % s, the length of the chain in increments 
 
-s = [-30:0.01:30]; % meters
-figure;
+s = [-50:0.01:50]; % meters
+
 
 ## http://www.beginningbeekeeping.com/BeeFaqs.htm
-## a bee has max 5/8 inch length = 1.58 cm
-## a bee weights a tenth of a gram = 1/10000
+## one bee has max 5/8 inch length = 1.58 cm
+## one bee weights a tenth of a gram = 1/10000
 ## lambda = nr of bees per meter * weigh of a bee
 
-subplot(1,1,1);
+pl=1;
 ## I needed to tweak the T to get into a workable catenary shape
 
 # dimensions
 hive_height = 40;
-hive_max_width = 30;
+hive_max_width = 45;  #+ wood 
 wood_width = 2.4;
-figure1 = figure;
 
-for a = 5
-  for w =1.33:0.1:4
-  a
-  w
+for a = 5:0.5:6
+  subplot(2,2,pl);
+
+  pl=pl+1
+  for w =1.33
+  title(sprintf("Params a,w: %0.2f,%0.2f",a,w));
   [x, y] = catenary(s, w, a);;
     # draw the catenary curve
     hold on;
     plot(x,y);
-    axis([-35 45 0 40]);
+    axis([-35 45 0 hive_height]);
     C = [x; y]';
     #save myfile.mat C;
 
@@ -49,16 +56,17 @@ for a = 5
            # add lines on the curve graph
             %% plot a line between two points using plot([x1,x2],[y1,y2])
             plot([-30,25],[h,h]);
-            text(26, h-(wood_width/2), sprintf("R,h(%1.2g, %1.2g)",C(i,1),h));
+            text(26, h-(wood_width/2), sprintf("R,h (%0.2f, %0.2f)",C(i,1),h));
+            
             hold on;           
            A = [A; new_row];
          endif  
         i++;
-      endwhile  
+      endwhile
+        
     end
-    A
-   save catenary.mat A
-   saveas(figure1,'finename.ext')  % here you save the figure
+    print -djpg catenaryHive.jpg
 
   end
+
 end
